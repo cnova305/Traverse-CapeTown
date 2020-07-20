75218d852b209f82772168cb42cc6b918c1bfe27
Christopher Juwao <njuwao@icloud.com>
2020-07-21 00:50:22 +0200

    Initial Commit

---
+++ Traverse CapeTown/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Traverse CapeTown
+//
+//  Created by Christopher Juwao on 2020/07/21.
+//  Copyright © 2020 Christopher Juwao. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
